ce3d3599391bdf4fb810274a2caad0965c08d888
Oskari Lehtonen <c7olehto@kc408mac02.tamk.ad>
2019-10-11 12:17:28 +0300

    Initial Commit

---
+++ weatherapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherapp
+//
+//  Created by Oskari Lehtonen on 11/10/2019.
+//  Copyright © 2019 Oskari Lehtonen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
