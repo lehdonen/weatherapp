17ac98f3033b0326b66e34be84beb4b8903c30ce
Oskari Lehtonen <c7olehto@kc408mac02.tamk.ad>
2019-10-11 13:36:55 +0300

    Add view controllers for tabs

---
+++ weatherapp/CitiesController.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesController.swift
+//  weatherapp
+//
+//  Created by Oskari Lehtonen on 11/10/2019.
+//  Copyright © 2019 Oskari Lehtonen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CitiesController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
