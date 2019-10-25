3610ad3477181e798486f433b679203370dc87f4
Oskari Lehtonen <c7olehto@kc408mac02.local>
2019-10-25 13:43:17 +0300

    Add locations to appdelegate

--- weatherapp/AppDelegate.swift
+++ weatherapp/AppDelegate.swift
@@ -7,62 +7,94 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    var reqController : APIController?
+    var locationManager : CLLocationManager?
+    var currentWeatherController : CurrentWeatherController?
+    var forecastController : ForecastController?
+    var citiesController : CitiesController?
+    
+    var geoCoder = CLGeocoder()
+    var locations : CLLocationCoordinate2D?
+    var location : CLLocation?
+    var placeMark: CLPlacemark?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let tabBarController = window?.rootViewController as! UITabBarController
+        
+        currentWeatherController = tabBarController.viewControllers![0] as! CurrentWeatherController
+        forecastController = tabBarController.viewControllers![1] as! ForecastController
+        citiesController = tabBarController.viewControllers![2] as! CitiesController
+        
+        reqController
+            = APIController()
+        
+        currentWeatherController?.reqController = self.reqController
+        
+        self.locationManager = CLLocationManager()
+        currentWeatherController?.locationManager = self.locationManager
+        self.locationManager!.delegate = self as! CLLocationManagerDelegate
+        locationManager!.requestAlwaysAuthorization()
+        self.locationManager!.startUpdatingLocation()
+        
         return true
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.locations = self.locationManager?.location?.coordinate
+        self.location = CLLocation(latitude: (self.locations?.latitude)!, longitude: (self.locations?.longitude)!)
+        geoCoder.reverseGeocodeLocation(location!, completionHandler: {(placemarks, error) -> Void in
+            var place: CLPlacemark!
+            place = placemarks?[0]
+            self.placeMark = placemarks?[0]
+            self.currentWeatherController?.setLocation(loc: self.locations!, place: place)
+        })
+        
+        self.locationManager!.stopUpdatingLocation()
+    }
+    
+    func newLocation(){
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager?.startUpdatingLocation()
+        }
+    }
+    
+    func application(_ application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
+        return true
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-    /*
-    func fetchUrl(url : String) {
-        let config = URLSessionConfiguration.default
-        
-        let session = URLSession(configuration: config)
-        
-        let url : URL? = URL(string: url)
-        
-        let task = session.dataTask(with: url!, completionHandler: doneFetching);
-        
-        // Starts the task, spawns a new thread and calls the callback function
-        task.resume();
-    }
-    
-    func doneFetching(data: Data?, response: URLResponse?, error: Error?) {
-        let resstr = String(data: data!, encoding: String.Encoding.utf8)
-        
-        // Execute stuff in UI thread
-        DispatchQueue.main.async(execute: {() in
-            NSLog(resstr!)
-        })
-    }
-    */
 }
 
